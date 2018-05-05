e546d9d9810a25bd3f7097aabed041f251138094
sbx_user1059 <sbx_user1059@ip-10-11-165-72.eu-west-2.compute.internal>
2018-05-05 12:12:47 +0000

    Sat May 05 12:12:47 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,37 +1,17 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 from collections import Counter
+from .product import products
 
-VALID_PRODUCTS = ['A', 'B', 'C', 'D', 'E', 'F']
 
 
-def total_price(na, nb, nc, nd, ne, nf):
-    # Get numbers of collections
-    na5 = na // 5
-    na3 = (na % 5) // 3
-    na1 = na - 5 * na5 - 3 * na3
-    nb = max(0, nb - ne // 2)
-    nb2 = nb // 2
-    nb1 = nb % 2
-    ne2 = ne // 2
-    nf3 = nf // 3
-    nf1 = nf % 3
-    print(nf3)
-
-    # Get costs
-    ca = na5 * 200 + na3 * 130 + na1 * 50
-    cb = nb2 * 45 + nb1 * 30
-    cc = nc * 20
-    cd = nd * 15
-    ce = ne * 40
-    cf = nf3 * 20 + nf1 * 10
-
-    return ca + cb + cc + cd + ce + cf
+def total_price(skus):
+    pass
 
 def extract_skus(skus):
     counter = Counter(skus)
     for k in counter.keys():
-        if k not in VALID_PRODUCTS:
+        if k not in products:
             raise ValueError('Invalid SKUs.')
     return dict(counter)
 
