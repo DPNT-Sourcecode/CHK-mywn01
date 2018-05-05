ce30a6169d82b11cdf660745032089ce4ba61da6
sbx_user1059 <sbx_user1059@ip-10-12-120-76.eu-west-2.compute.internal>
2018-05-05 10:00:41 +0000

    Sat May 05 10:00:41 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,6 +1,11 @@
-
+import re
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+def extract_skus(skus):
+    sku_list = re.findall(r'\d*[A-D]', skus)
+    return tuple(sku_list)
+
+
 def checkout(skus):
     raise NotImplementedError()
--- test/solutions/test_checkout.py
+++ test/solutions/test_checkout.py
@@ -3,10 +3,10 @@ import pytest
 from lib.solutions.checkout import checkout, extract_skus
 
 @pytest.mark.parametrize('skus, expected', [
-    ('A', ('A',)),
-    ('AB', ('A', 'B',)),
-    ('2AB', ('2A', 'B',)),
-    ('2A3C', ('2A', '3C')),
+    ('A', {'A': 1}),
+    ('AB', {'A': 1, 'B': 1}),
+    ('2AB', {'A': 2, 'B': 1}),
+    ('2A3C', {'A': 2, 'C': 3}),
 ])
 def test_extract_skus(skus, expected):
     extract = extract_skus(skus)
