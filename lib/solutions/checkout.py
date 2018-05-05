7ab920a5bfb7e9842c1c1dce27b0482cd441f774
sbx_user1059 <sbx_user1059@ip-10-11-166-167.eu-west-2.compute.internal>
2018-05-05 13:07:26 +0000

    Sat May 05 13:07:26 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,9 +1,19 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 from collections import Counter
+import itertools
 from .product import products
 
 
+def group_discount_cost(skus):
+    group_items = ''
+    for i in 'STXYZ':
+        group_items += i * skus[i]
+    n_group = len(group_items)
+    n_extra = n_group % 3
+    extra_combos = itertools.combinations(group_items, n_extra)
+    extra_prices = [sum([products[i].get_price(skus) for i in ec]) for ec in extra_combos]
+    return (n_group // 3) * 45 + min(extra_prices)
 
 def total_price(skus):
     cost = 0
