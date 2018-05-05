59984eadc72288d2592f93c7d50d9f1aeddfe9fc
sbx_user1059 <sbx_user1059@ip-10-12-120-76.eu-west-2.compute.internal>
2018-05-05 10:03:42 +0000

    Sat May 05 10:03:42 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -4,8 +4,15 @@ import re
 # skus = unicode string
 def extract_skus(skus):
     sku_list = re.findall(r'\d*[A-D]', skus)
-    return tuple(sku_list)
+    if sum(sku_list) != skus:
+        raise ValueError('Invalid SKUs')
+    quantity_items = [(s[:-1], s[-1]) for s in sku_list]
+    item_quantities = {q[1]: q[0] for q in quantity_items}
+    return item_quantities
 
 
 def checkout(skus):
-    raise NotImplementedError()
+    try:
+        skus = extract_skus(skus)
+    except ValueError:
+        return -1
--- test/solutions/test_checkout.py
+++ test/solutions/test_checkout.py
@@ -7,6 +7,7 @@ from lib.solutions.checkout import checkout, extract_skus
     ('AB', {'A': 1, 'B': 1}),
     ('2AB', {'A': 2, 'B': 1}),
     ('2A3C', {'A': 2, 'C': 3}),
+    ('2A3D2A', {'A': 4, 'D': 3})
 ])
 def test_extract_skus(skus, expected):
     extract = extract_skus(skus)
